69501dd978d2f4a3d64a77b5b4c8d298d640ce94
Yaroslav <31samael13@gmail.com>
2020-06-21 23:45:47 +0700

    upload_photo

--- server/app.js
+++ server/app.js
@@ -39,12 +39,12 @@ app.use("/refresh-tokens", Users.refreshToken);
 
 aws.config.region="eu-north-1";
 
-const s3 = new aws.S3({})
+const s3 = new aws.S3()
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: '3drealtor-images',
+    bucket: process.env.S3_BUCKET,
     acl: 'public-read',
     metadata: function (req, file, cb) {
       cb(null, {fieldName: file.fieldname});
