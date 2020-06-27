19ec2b1517d833b1fed7cc4960331df55aadfeee
Yaroslav <31samael13@gmail.com>
2020-06-27 16:39:27 +0700

    property

--- server/app.js
+++ server/app.js
@@ -135,7 +135,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 
   app.get ('*', function (req, res) { 
-    res.sendFile (path.join (__dirname, 'build', 'index.html')) ; 
+    res.sendFile (path.join (__dirname, 'public', 'index.html')) ; 
   }); 
 }
 
